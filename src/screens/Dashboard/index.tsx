4754150cb3851ffd9382fbb5202948d4797de715
Jimmy Bastos <jimmybastosdev@gmail.com>
2020-11-29 21:00:06 -0300

    List available barbers in the dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,27 +1,133 @@
-import React from 'react'
-import { View, Button } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
 
-import { useAuth } from '../../hooks/auth'
+import { useAuth } from "../../hooks/auth";
+
+import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+  ProvidersListTitle,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderInfo,
+  ProviderName,
+  ProviderMeta,
+  ProviderMetaText
+ } from "./styles";
+
+import Icon from 'react-native-vector-icons/Feather'
+
+export interface Provider {
+  id: string
+  name: string
+  avatar_url: string
+}
+
+const Dashboard: React.FC = () => {
+  const { user, signOut } = useAuth()
+  const { navigate } = useNavigation()
+
+  const [providers, setProviders] = useState<Provider[]>([])
+
+  useEffect(
+    function loadProviders () {
+      api.get<Provider[]>('/providers').then(
+        ({data: providers}) => {
+          setProviders(providers)
+        }
+      )
+    }, []
+  )
+
+  const goToProfile = useCallback(
+    () => {
+      navigate("Profile")
+    },
+    [navigate]
+  )
+
+  const goToCreateAppointment = useCallback(
+    (providerId: string) => {
+      navigate("CreateAppointment", { providerId })
+    },
+    [navigate]
+  )
 
 
-const Routes: React.FC = () => {
-  const { signOut } = useAuth()
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
-      <Button
-        title="Sair"
-        onPress={signOut}
-      />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <UserName>
+            {user.name}
+          </UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={goToProfile}>
+          <UserAvatar source={{uri: user.avatar_url}} />
+        </ProfileButton>
+      </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        ListHeaderComponent={
+          <ProvidersListTitle>
+            Cabeleireiros
+          </ProvidersListTitle>
+        }
+        renderItem={
+          ({ item: provider }) => (
+            <ProviderContainer
+              onPress={() => goToCreateAppointment(provider.id)}
+            >
+              <ProviderAvatar source={{uri: provider.avatar_url}} />
+
+              <ProviderInfo>
+                <ProviderName>
+                  {provider.name}
+                </ProviderName>
+
+                <ProviderMeta>
+                  <Icon
+                    name="calendar"
+                    size={14}
+                    color="#ff9000"
+                  />
+                  <ProviderMetaText>
+                    Segunda à sexta
+                  </ProviderMetaText>
+                </ProviderMeta>
+
+                 <ProviderMeta>
+                  <Icon
+                    name="clock"
+                    size={14}
+                    color="#ff9000"
+                  />
+                  <ProviderMetaText>
+                    8h às 18h
+                  </ProviderMetaText>
+                </ProviderMeta>
+              </ProviderInfo>
+            </ProviderContainer>
+          )
+        }
+      >
+
+      </ProvidersList>
+    </Container>
   )
 }
 
 
-export default Routes
+export default Dashboard
